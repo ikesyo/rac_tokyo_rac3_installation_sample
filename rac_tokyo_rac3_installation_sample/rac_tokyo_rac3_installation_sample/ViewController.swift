cde83c2b6c6e8ef945902c2662e3b6f435065851
Syo Ikeda <suicaicoca@gmail.com>
2014-10-18 09:36:32 +0900

    Add empty Xcode project

---
+++ rac_tokyo_rac3_installation_sample/rac_tokyo_rac3_installation_sample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  rac_tokyo_rac3_installation_sample
+//
+//  Created by Syo Ikeda on 10/18/14.
+//  Copyright (c) 2014 Syo Ikeda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
